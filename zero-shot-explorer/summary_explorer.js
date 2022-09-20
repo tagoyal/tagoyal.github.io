e8c1ce63cb34a2b29aefcc7026b0e17a7827aeb4
Tanya Goyal <tanyagoyal@utexas.edu>
2022-09-20 05:37:14 -0500

    barebones demo v1

--- zero-shot-explorer/summary_explorer.js
+++ zero-shot-explorer/summary_explorer.js
@@ -11,7 +11,7 @@ $(document).on('focusout', '.dropdown', function (e) {
 $(document).on("click", '.dropdown-menu-model li', function (e) {
     $(this).parents('.dropdown-menu-model').find('span').text($(this).text());
     document.getElementById('dataset-current').innerHTML = $(this).attr('dataset-name');
-    console.log('her')
+    console.log($(this).attr('dataset-name'))
 });
 
 $(document).on("click", '.dropdown-menu-ID li', function (e) {
@@ -22,24 +22,36 @@ $(document).on("click", '.dropdown-menu-ID li', function (e) {
 
 
 
-
-
 // handle click and add class
 $(document).on("click", '#random-sample', function (e){
 
+  //dataset_name = document.getElementById("dataset-current").innerHTML;
+  dataset_name = 'cnn';
   var articlecontainer = $("#article-display-box");
-  var btn = $("#btn1");
+  var pegasuscontainer = $("#pegasus-summary");
+  var briocontainer = $("#brio-summary");
+  var t0container = $("#t0-summary");
+  var gpt3container = $("#gpt3-summary");
+
+  var url = "https://tagoyal.github.io/zero-shot-explorer/" + dataset_name + ".jsonl"
+  console.log(dataset_name)
   $.ajax({
-    url: "https://raw.githubusercontent.com/alv2017/DataSets/master/Europe/europe-capital-cities.json",
+    url: url,
     dataType: "json"
   }).done(function(result) {
-    let id = Math.floor(Math.random() * 44);
-    let country = result['European Capitals'][id]['capital_name'];
-    let latitude = result['European Capitals'][id]['latitude'];
-    let longitude = result['European Capitals'][id]['longitude'];
+    let id = Math.floor(Math.random() * 2);
+    let article = result[id]['article'];
+    let brio = result[id]['brio'];
+    let pegasus = result[id]['pegasus'];
+    let t0 = result[id]['t0'];
+    let gpt3 = result[id]['gpt3'];
 
-    let dstring = "Country name: " + country + ", Latitude: " + latitude + ", Longitude: " + longitude;
-    articlecontainer.text(dstring);
+    articlecontainer.text(article);
+    pegasuscontainer.text('Pegasus Summary: ' + pegasus);
+    briocontainer.text('BRIO Summary: ' + brio);
+    t0container.text('T0 Summary: ' + t0);
+    gpt3container.text('GPT-3 Summary: ' + gpt3);
+
   });
 
 });
